b4205caea6949b14446af3305cd9b1d35367bd2b
chigivera <ayman.benchamkha@gmail.com>
2024-04-20 16:12:54 +0100

    nice

--- src/Data/ajouterDetails.js
+++ src/Data/ajouterDetails.js
@@ -1,146 +1,88 @@
-const form = document.getElementById("myForm");
-const chiffre = document.getElementById("CA");
-const effictif = document.getElementById("effictif");
-const surface = document.getElementById("surface");
+const anneeValue = document.getElementById("annee").value;
+const chiffreValue = document.getElementById("CA").value;
+const effictifValue = document.getElementById("effictif").value;
+const surfaceValue = document.getElementById("surface").value;
 const submitBtn = document.querySelector(".submit");
 const details = document.getElementById("data");
-const modal = document.getElementById("userForm");
-const modalTitle = document.querySelector("#userForm .modal-title");
-const newUserBtn = document.querySelector(".newDetail");
 
-magasins = localStorage.getItem('localMagasins') ? JSON.parse(localStorage.getItem('localMagasins')) : [];
-console.log(magasins)
+
+const magasins = localStorage.getItem('localMagasins') ? JSON.parse(localStorage.getItem('localMagasins')) : [];
 // Get the current URL
 const urlParams = new URLSearchParams(window.location.search);
 
 // Get individual parameter values
 const magasinNom = urlParams.get('magasin');
 const magasinLocation = urlParams.get('location');
-console.log(magasinLocation )
-console.log(magasinNom )
 
 const selectedMagasin = magasins.filter(magasin => magasin.nom == magasinNom && magasin.location == magasinLocation).map(magasin => magasin.donnees)[0]
-const magasinId = magasins.filter(magasin => magasin.nom == magasinNom && magasin.location == magasinLocation)[0].id
+const magasinId = magasins.filter(magasin => magasin.nom == magasinNom && magasin.location == magasinLocation)[0]
 console.log(selectedMagasin)
 
-let isEdit = false;
-let editId;
+window.addEventListener("DOMContentLoaded",()=>{
 
-showInfo();
-
-newUserBtn.addEventListener('click', () => {
-    submitBtn.innerText = 'Submit';
-    modalTitle.innerText = "Fill the Form";
-    isEdit = false;
-    form.reset();
-});
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const newDetail = {
-        CA: chiffre.value,
-        effictif: effictif.value,
-        surface: surface.value
-    };
-
-    getData.push(newDetail);
-    localStorage.setItem('magazinsDetails', JSON.stringify(getData));
-    showInfo();
-});
-
-console.log(selectedMagasin)
-function showInfo() {
-  // Clear previous details
   details.innerHTML = "";
-
+  
   selectedMagasin.forEach((donnee, index) => {
-    console.log(donnee.annee)
       const row = document.createElement("tr");
       row.innerHTML = `
-          
-          <td>${donnee.annee}</td>
-          <td>${donnee.CA}</td>
-          <td>${donnee.effictifs}</td>
+      <td>${donnee.annee}</td>
+      <td>${donnee.CA}</td>
+          <td>${donnee.effectifs}</td>
           <td>${donnee.surface}</td>
           <td>
-              <button class="btn btn-primary" onclick="editInfo(${index}, '${donnee.CA}', '${donnee.effictif}', '${donnee.surface}')" data-bs-toggle="modal" data-bs-target="#userForm"> <i class="bi bi-pencil-square"></i></button>
-              <button class="btn btn-danger" onclick="deleteInfo(${index})"><i class="bi bi-trash"></i></button>
-          </td>
-      `;
-      details.appendChild(row);
+              <button class="btn btn-primary"> <i class="bi bi-pencil-square"></i></button>
+              <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
+              </td>
+              `;
+              details.appendChild(row);
+            });
+
+      const editBtn = Array.from(document.querySelectorAll(".bi.bi-pencil-square"))
+      
+      editBtn.forEach(btn=>btn.addEventListener("click",()=>{
+      const row =  btn.closest("tr")
+      const data = Array.from(row.querySelectorAll("td"))
+      const anneeValue = data[0].innerText
+      const chiffreValue = data[1].innerText
+      const effectifValue = data[2].innerText
+      const surfaceValue = data[3].innerText
+      console.log("Annee Value:", anneeValue);
+console.log("Chiffre_Affaire Value:", chiffreValue);
+console.log("Effectif Value:", effectifValue);
+console.log("Surface Value:", surfaceValue);
+      }))
+            
+})  
+
+document.getElementById("ajouterModal").addEventListener("submit", (e) => {
+  e.preventDefault();
+  magasins = localStorage.getItem('localMagasins') ? JSON.parse(localStorage.getItem('localMagasins')) : [];
+  const selectedMagasin = magasins.filter(magasin => magasin.nom == magasinNom && magasin.location == magasinLocation).map(magasin => magasin.donnees)[0]
+  
+  // Retrieve input field values
+  const anneeValue = parseInt(document.getElementById("annee").value);
+    const chiffreValue = parseFloat(document.getElementById("CA").value);
+    const effictifValue = parseInt(document.getElementById("effictif").value);
+    const surfaceValue = parseFloat(document.getElementById("surface").value);
+
+  // Log input field values
+  console.log("Annee Value:", anneeValue);
+  console.log("Chiffre_Affaire Value:", chiffreValue);
+  console.log("Effictif Value:", effictifValue);
+  console.log("Surface Value:", surfaceValue);
+
+  // Push values into selectedMagasin array
+  selectedMagasin.push({
+      annee: anneeValue,
+      CA: chiffreValue,
+      effectifs: effictifValue,
+      surface: surfaceValue
   });
-}
-function editInfo(index, Chiffre, Effictif, Surface) {
-  isEdit = true;
-  editId = index;
 
-  chiffre.value = Chiffre;
-  effictif.value = Effictif;
-  surface.value = Surface;
-
-  submitBtn.innerText = "Update";
-  modalTitle.innerText = "Update The Form";
-  submitBtn.removeEventListener('click', submitForm);
-  submitBtn.addEventListener('click', updateInfo);
-}
-
-function updateInfo() {
-  const CAValue = chiffre.value;
-  const effictifValue = effictif.value;
-  const surfaceValue = surface.value;
-
-  const information = {
-    CA: CAValue,
-    effictif: effictifValue,
-    surface: surfaceValue
-  };
-
-  getData[editId] = information;
-  localStorage.setItem('magazinsDetails', JSON.stringify(getData));
-  form.reset();
-
-  isEdit = false;
-  submitBtn.innerText = "Submit";
-  modalTitle.innerText = "Fill the Form";
-  submitBtn.removeEventListener('click', updateInfo);
-  window.location.reload();
-
-  showInfo();
-}
-
-function submitForm(event) {
-  event.preventDefault();
-
-  const CAValue = chiffre.value;
-  const effictifValue = effictif.value;
-  const surfaceValue = surface.value;
-
-  const newDetail = {
-    CA: CAValue,
-    effictif: effictifValue,
-    surface: surfaceValue
-  };
-
-  getData.push(newDetail);
-  
-  localStorage.setItem('magazinsDetails', JSON.stringify(getData));
-  
-  form.reset();
-  showInfo();
-  
-}
+  // Log selectedMagasin array after pushing
+  console.log("selectedMagasin:", selectedMagasin);
+  localStorage.setItem("localMagasins",JSON.stringify(magasins))
+});
 
 
 
-
-
-
-// Fonction pour supprimer les détails
-function deleteInfo(index) {
-  if (confirm("Are you sure want to delete?")) {
-      getData.splice(index, 1);
-      localStorage.setItem("magazinsDetails", JSON.stringify(getData));
-      showInfo();
-  }
-}
